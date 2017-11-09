227270feb80074c24a92eb777724c2afa6e27e56
dubaua <dubaua@gmail.com>
2017-11-09 14:37:13 +0500

    Nothign works

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
 import styled from "styled-components";
-import logo from "./logo.svg";
 import "./circular-air.css";
 import "flexboxgrid2";
 import Header from "./Header";
-
+ad;
 const Page = styled.header`
   font-family: "Circular Air", Helvetica, Arial, sans-serif;
   color: #383838;
